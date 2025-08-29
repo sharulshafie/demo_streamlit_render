20cd84c3c71dec29904405c718756078c7e7ef6a
sharulariffin <sharul.ariffin13@gmail.com>
2025-08-30 05:53:11 +0800

    feat : execute the db engine and view the data

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
 import streamlit as st
+import psycopg2
+import pandas as pd
+from db import get_connection
 
 st.title("Hello world")
+
+
+# Initialize connection from render db
+conn = get_connection()
+cur = conn.cursor()
+
+# Fetch data form render db
+cur.execute("SELECT * FROM users;")
+data = cur.fetchall()
+
+# Call the columns from db
+columns = [desc[0] for desc in cur.description]
+
+# Display the data
+df = pd.DataFrame(data, columns=columns)
+st.dataframe(df)
